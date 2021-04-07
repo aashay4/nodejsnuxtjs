de4042c1c3d3bab7198a61f79945170472161e3f
Aashay <aashay.trivedi1@gmail.com>
2021-04-07 16:52:11 +0530

    first

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,4 @@
-/*!
 
- =========================================================
- * Nuxt Black Dashboard PRO - v1.0.0
- =========================================================
-
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- */
 export default {
   mode: 'universal',
   /*
